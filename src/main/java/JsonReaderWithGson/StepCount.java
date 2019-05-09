95cc8499b7ffa15e67ac5aa3bc31a919f694ad46
xFus10n <razumilovrenat@inbox.lv>
2019-05-09 19:25:46 +0300

    Json with Gson (without intVal value)

--- src/main/java/JsonReaderWithGson/StepCount.java
+++ src/main/java/JsonReaderWithGson/StepCount.java
@@ -4,20 +4,20 @@ import java.util.List;
 
 public class StepCount {
 
-    private List<Bucket> buckets;
+    private Bucket[] bucket;
 
-    public List<Bucket> getBuckets() {
-        return buckets;
+    public Bucket[] getBucket() {
+        return bucket;
     }
 
-    public void setBuckets(List<Bucket> buckets) {
-        this.buckets = buckets;
+    public void setBucket(Bucket[] bucket) {
+        this.bucket = bucket;
     }
 
-    @Override
-    public String toString() {
-        return buckets.get(0).getStartTimeMillis() + " - " + buckets.get(0).getEndTimeMillis() + " - "
-                + buckets.get(0).getDataset().get(0).getPoint().get(0).getOriginDataSourceId()
-                + buckets.get(0).getDataset().get(0).getPoint().get(0).getValues().get(0).getValue().get(0).getIntVal().get(1);
-    }
+//    @Override
+//    public String toString() {
+//        return bucket.getStartTimeMillis() + " - " + bucket.getEndTimeMillis() + " - "
+//                + bucket.getDataset().get(0).getPoint().get(0).getOriginDataSourceId()
+//                + bucket.getDataset().get(0).getPoint().get(0).getValues().get(0).getValue().get(0).getIntVal().get(1);
+//    }
 }
--- src/main/java/RestExample/Greeting.java
+++ src/main/java/RestExample/Greeting.java
@@ -1,16 +1,12 @@
 package RestExample;
 
 public class Greeting {
-    //private final long id;
+
     private final String content;
 
     public Greeting(String content) {
         this.content = content;
     }
-//
-//    public long getId() {
-//        return id;
-//    }
 
     public String getContent() {
         return content;
